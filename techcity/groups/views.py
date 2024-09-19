252250e0f1b35480a123b30c9b0b13a4984dd66f
Matt Layman <matthewlayman@gmail.com>
2024-09-18 23:45:15 -0400

    Add the group event card rendering.

--- techcity/groups/views.py
+++ techcity/groups/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+
+from techcity.events.models import Event
 
 from .models import Group
 
 
 def group_detail(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    now = timezone.now()
     context = {
-        "group": get_object_or_404(Group, slug=slug),
-        # TODO: need `events` in context.
+        "group": group,
+        "events": Event.objects.filter_around(now).filter(group=group),
+        "now": now,
     }
     return render(request, "groups/detail.html", context)
 
