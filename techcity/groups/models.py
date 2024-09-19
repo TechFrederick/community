8e7dd205413e95c18278622a24e0a439aca2b325
Matt Layman <matthewlayman@gmail.com>
2024-09-18 23:59:08 -0400

    Clean up the group page.

--- techcity/groups/models.py
+++ techcity/groups/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from techcity.core.frontend import tailwindify_html
+
 
 class Group(models.Model):
     """A group represents any collection of people.
@@ -10,6 +12,7 @@ class Group(models.Model):
     name = models.CharField(max_length=128)
     slug = models.SlugField()
     url = models.URLField(max_length=256)
+    description = models.TextField()
     teaser = models.TextField()
     color = models.CharField(
         max_length=32,
@@ -29,3 +32,8 @@ class Group(models.Model):
     @property
     def hero_image(self):
         return f"images/group-hero/{self.slug}.png"
+
+    @property
+    def html_description(self):
+        # Wrap in a div because a root node is expected to format properly.
+        return tailwindify_html(f"<div>{self.description}</div>")
--- techcity/groups/views.py
+++ techcity/groups/views.py
@@ -9,9 +9,10 @@ from .models import Group
 def group_detail(request, slug):
     group = get_object_or_404(Group, slug=slug)
     now = timezone.now()
+    events = Event.objects.filter_around(now).filter(group=group).order_by("-start_at")
     context = {
         "group": group,
-        "events": Event.objects.filter_around(now).filter(group=group),
+        "events": events,
         "now": now,
     }
     return render(request, "groups/detail.html", context)
