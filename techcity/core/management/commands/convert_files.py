72b8ff871e485118ed12520f8134c190854eab0b
Matt Layman <matthewlayman@gmail.com>
2024-09-21 14:48:46 -0400

    Process venues and link to events.

--- techcity/core/management/commands/convert_files.py
+++ techcity/core/management/commands/convert_files.py
@@ -1,10 +1,20 @@
 from django.core.management.base import BaseCommand
 
-from techcity.events.models import Event
+from techcity.events.models import Event, Venue
 from techcity.groups.models import Group
 from techcity.models import EventListFilterOptions
 from techcity.services.events.repository import EventRepository
 from techcity.services.groups.repository import GroupRepository
+
+
+def clean_address(address):
+    """Clean the address.
+
+    Some of the addresses that come from Meetup are trash. Since address is
+    the unique key, it needs to be cleaned up to make sure that we match
+    places that should be equal.
+    """
+    return address.strip().rstrip(",")
 
 
 class Command(BaseCommand):
@@ -12,7 +22,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Converting data files to database entries...")
-        # TODO: process venues
         # TODO: pull meetup info about groups and events
         # TODO: pull wordpress info out of techfrederick group
         group_repo = GroupRepository()
@@ -32,6 +41,26 @@ class Command(BaseCommand):
             groups[group.slug] = db_group
 
         event_repo = EventRepository()
+        venues = {}
+        for event in sorted(
+            event_repo.list(EventListFilterOptions()), key=lambda e: e.start_at
+        ):
+            if event.venue:
+                cleaned_address = clean_address(event.venue.address)
+                if cleaned_address not in venues:
+                    print(f"Venue {cleaned_address}...")
+                    defaults = {
+                        "city": event.venue.city.strip(),
+                        "state": event.venue.state,
+                        "zip": event.venue.zip,
+                        "lat": event.venue.lat,
+                        "long": event.venue.lon,
+                    }
+                    venues[cleaned_address], _ = Venue.objects.update_or_create(
+                        address=cleaned_address, defaults=defaults
+                    )
+
+        event_repo = EventRepository()
         for event in sorted(
             event_repo.list(EventListFilterOptions()), key=lambda e: e.start_at
         ):
@@ -42,6 +71,9 @@ class Command(BaseCommand):
                 "start_at": event.start_at,
                 "end_at": event.end_at,
                 "description": event.description,
-                # TODO: link to venue
             }
+            if event.venue:
+                cleaned_address = clean_address(event.venue.address)
+                defaults["venue"] = venues.get(cleaned_address)
+
             Event.objects.update_or_create(url=event.link, defaults=defaults)
