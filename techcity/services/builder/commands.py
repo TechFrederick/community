7332072098d0f9254240f145ced51fb7744e29ed
Matt Layman <matthewlayman@gmail.com>
2024-09-23 02:03:04 -0400

    Tear down more unnecessary stuff.

--- techcity/services/builder/commands.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-from techcity.events import BuildTriggered
-
-
-def build(ctx: typer.Context) -> None:
-    """Build the web UI by rendering all available content."""
-    pubsub = ctx.obj["pubsub"]
-    pubsub.publish(BuildTriggered())
