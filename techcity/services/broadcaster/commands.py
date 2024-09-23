1c448e73d4b14fd8c7a3b240cc4b0e0d7ebe74e4
Matt Layman <matthewlayman@gmail.com>
2024-09-23 01:51:36 -0400

    Remove broadcaster service.

--- techcity/services/broadcaster/commands.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-from techcity.events import BroadcastTriggered
-
-
-def broadcast(ctx: typer.Context) -> None:
-    """Send out broadcast messages about any upcoming event."""
-    pubsub = ctx.obj["pubsub"]
-    pubsub.publish(BroadcastTriggered())
