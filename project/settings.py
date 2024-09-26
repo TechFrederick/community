80f602a5b0bc8a2fdffb5cf16262be4149dcac5a
Matt Layman <matthewlayman@gmail.com>
2024-09-26 00:40:32 -0400

    Add static file handling.

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,6 +117,11 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- project/testing_settings.py
+++ project/testing_settings.py
@@ -7,3 +7,15 @@ DATABASES = {
         "NAME": ":memory:",
     }
 }
+
+STORAGES = {
+    "staticfiles": {
+        # Whitenoise does not play well with tests
+        # because tests don't run collectstatic.
+        # Override back to the default storage for testing.
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+# This eliminates the warning about a missing staticfiles directory.
+WHITENOISE_AUTOREFRESH = True
