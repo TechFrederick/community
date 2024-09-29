21443152a31591c981a6dc2ba8b788961b756253
Matt Layman <matthewlayman@gmail.com>
2024-09-29 17:11:48 -0400

    Add Kamal config.

--- project/settings.py
+++ project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": env.path("DB_DIR", BASE_DIR) / "db.sqlite3",
     }
 }
 
