94a1fdc21793e46aaefc26fff4bfa6e62dc79026
greg-meinecke <48494004+greg-meinecke@users.noreply.github.com>
2024-10-29 23:04:28 -0400

    Create bootstrapping ability: Issue-85 (#90)
    * create groups endpoint
    
    * add pre-commit as dev dependency
    
    * update make bootstrap
    
    * add api tests
    
    * update README.md for pre-commit
    
    * address pr comments

---
+++ techcity/api/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+
+from techcity.groups.tests.factories import GroupFactory
+
+
+class TestGetGroups:
+    def test_ok(self, client):
+        GroupFactory()
+
+        response = client.get(reverse("api:get_groups"))
+
+        assert response.status_code == 200
+
+        response_payload = response.json()
+        assert isinstance(response_payload, list)
+        assert len(response_payload) > 0
---
+++ techcity/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import get_groups
+
+app_name = "api"
+urlpatterns = [
+    path("v1/groups", get_groups, name="get_groups"),
+]
