3a900b89a20eac261467426cac9190b371a2898f
Matt Layman <matthewlayman@gmail.com>
2024-11-15 19:25:58 -0500

    Show group page events to display Last and Upcoming events.
    Fixes #100

--- techcity/groups/views.py
+++ techcity/groups/views.py
@@ -13,10 +13,20 @@ from .models import Group
 def group_detail(request, slug):
     group = get_object_or_404(Group, slug=slug)
     now = timezone.now()
-    events = Event.objects.filter_around(now).filter(group=group).order_by("-start_at")
+    upcoming_events = (
+        Event.objects.filter_timeframe(from_datetime=now)
+        .filter(group=group)
+        .order_by("start_at")
+    )
+    last_events = (
+        Event.objects.filter_timeframe(to_datetime=now)
+        .filter(group=group)
+        .order_by("-start_at")
+    )
     context = {
         "group": group,
-        "events": events,
+        "upcoming_events": upcoming_events[:2],
+        "last_events": last_events[:2],
         "now": now,
     }
     return render(request, "groups/detail.html", context)
